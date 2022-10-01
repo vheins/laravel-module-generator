a9a4786389d7662aaa621e22b311dbfc8f60e7a3
Muhammad Rheza Alfin <rheza.ids@gmail.com>
2022-10-02 00:17:29 +0700

    Update Providers

---
+++ Providers/LaravelModuleGeneratorServiceProvider.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace Vheins\LaravelModuleGenerator\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Vheins\LaravelModuleGenerator\Console\CreateApiCrud;
+use Vheins\LaravelModuleGenerator\Console\CreateModule;
+use Vheins\LaravelModuleGenerator\Console\CreateModuleAction;
+use Vheins\LaravelModuleGenerator\Console\CreateModuleController;
+use Vheins\LaravelModuleGenerator\Console\CreateModuleMigration;
+use Vheins\LaravelModuleGenerator\Console\CreateModuleModel;
+use Vheins\LaravelModuleGenerator\Console\CreateModuleRequest;
+use Vheins\LaravelModuleGenerator\Console\CreateModuleSub;
+use Vheins\LaravelModuleGenerator\Console\CreateModuleVueComponentForm;
+use Vheins\LaravelModuleGenerator\Console\CreateModuleVueComponentLink;
+use Vheins\LaravelModuleGenerator\Console\CreateModuleVueComponentTab;
+use Vheins\LaravelModuleGenerator\Console\CreateModuleVuePageCreate;
+use Vheins\LaravelModuleGenerator\Console\CreateModuleVuePageIndex;
+use Vheins\LaravelModuleGenerator\Console\CreateModuleVuePageView;
+use Vheins\LaravelModuleGenerator\Console\CreateModuleVuePageStore;
+use Vheins\LaravelModuleGenerator\Console\CreatePermission;
+
+
+class LaravelModuleGeneratorServiceProvider extends ServiceProvider`
+{
+    /**
+     * @var string $moduleName
+     */
+    protected $moduleName = 'LaravelModuleGenerator';
+
+    /**
+     * @var string $moduleNameLower
+     */
+    protected $moduleNameLower = 'laravel-module-generator';
+
+    /**
+     * Boot the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->registerConfig();
+        $this->configureCommands();
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+    }
+
+    public function configureCommands()
+    {
+        if (!$this->app->runningInConsole()) {
+            return;
+        }
+
+        $this->commands([
+            CreateApiCrud::class,
+            CreateModule::class,
+            CreateModuleAction::class,
+            CreateModuleController::class,
+            CreateModuleMigration::class,
+            CreateModuleModel::class,
+            CreateModuleRequest::class,
+            CreateModuleSub::class,
+            CreateModuleVueComponentForm::class,
+            CreateModuleVueComponentLink::class,
+            CreateModuleVueComponentTab::class,
+            CreateModuleVuePageCreate::class,
+            CreateModuleVuePageIndex::class,
+            CreateModuleVuePageView::class,
+            CreateModuleVuePageStore::class,
+            CreatePermission::class,
+        ]);
+    }
+
+    /**
+     * Register config.
+     *
+     * @return void
+     */
+    protected function registerConfig()
+    {
+        $this->publishes([
+            __DIR__ . '/../config.php' => config_path($this->moduleNameLower . '.php'),
+        ], 'config');
+        $this->mergeConfigFrom(
+            __DIR__ . '/../config.php',
+            $this->moduleNameLower
+        );
+    }
+
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return [];
+    }
+}
